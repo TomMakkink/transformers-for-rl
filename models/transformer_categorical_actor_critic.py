Fix: GTrXL attention mask fix

models/transformer_categorical_actor_critic.py
@@ -15,6 +15,7 @@ class TransformerActorCritic(nn.Module):
         super().__init__()
         obs_dim = observation_space.shape[1]
         act_dim = action_space.n
+        self.transformer_output = nn.Linear(transformer_config['d_model'], transformer_config['output_dim'])
         # self.obs_linear = nn.Sequential(
         #     nn.Linear(obs_dim, 32), 
         #     nn.ReLU(), 
@@ -30,12 +31,11 @@ class TransformerActorCritic(nn.Module):
             action: [seq_len]               
         """
         # obs = self.obs_linear(obs)
-        # [seq_len, batch_size, features]
         obs = obs.unsqueeze(1)
         # print(f"Shape of obs: {obs.shape}")
         obs = self.transformer(obs)[-1]
-        # print(f"Obs output shape: {obs.shape}")
-        # obs = obs.permute(1, 0, 2)
+        # print(f"Shape of obs: {obs.shape}")
+        obs = self.transformer_output(obs)
         logits = self.actor(obs)
         action_dist = Categorical(logits=logits)
         logp = action_dist.log_prob(action)
@@ -52,12 +52,13 @@ class TransformerActorCritic(nn.Module):
             # obs = self.obs_linear(obs)
             # Transformer input shape: [seq_len, batch_size, features]
             obs = obs.unsqueeze(1)
-            obs = self.transformer(obs)[-1]             # Take last element of sequence
-            # Linear layer: [batch_size, *, features])
+            obs = self.transformer(obs)[-1]
+            # print(f"Shape of obs: {obs.shape}")
+            obs = self.transformer_output(obs)
+            # print(f"Shape of obs: {obs.shape}")
             logits = self.actor(obs)
             action_dist = Categorical(logits=logits)
             action = action_dist.sample()
             logp = action_dist.log_prob(action)
             value = torch.squeeze(self.critic(obs), -1)
-        # Only return last action/value/logp
         return action.cpu().numpy(), value.cpu().numpy(), logp.cpu().numpy()

transformers/attention_layer.py
@@ -395,10 +395,10 @@ class GTrXLBlock(nn.Module):
         self.pos_wise_mlp = PositionWiseMLP(d_model, dim_mlp, dropout)
         self.relu = nn.ReLU(inplace=True)
 
-    def forward(self, inputs:Tensor, r:Tensor, u:Tensor, v:Tensor, mem:Tensor=None):
+    def forward(self, inputs:Tensor, r:Tensor, u:Tensor, v:Tensor, attn_mask=None, mem:Tensor=None):
         # Attention
         x = inputs 
-        y = self.attention(inputs, r, u, v, mem)
+        y = self.attention(inputs, r, u, v, attn_mask, mem)
         y = self.layer_norm_1(y)
         y = self.gated_layer_1([x,y])
         

transformers/positional_encoding_layer.py
@@ -57,7 +57,7 @@ class AbsolutePositionalEncoding(nn.Module):
 
 class RelativePositionalEncoding(nn.Module):
     def __init__(self, demb):
-        super(PositionalEmbedding, self).__init__()
+        super(RelativePositionalEncoding, self).__init__()
         self.demb = demb
         inv_freq = 1 / (10000 ** (torch.arange(0.0, demb, 2.0) / demb))
         self.register_buffer('inv_freq', inv_freq)

transformers/transformer_gtr_xl.py
@@ -117,7 +117,7 @@ class GTrXL(nn.Module):
             core_out = layer(core_out, pos_emb, self.u, self.v, attn_mask=attn_mask, mem=mem_i)
             hids.append(core_out)
 
-        core_out = self.drop(core_out)
+        core_out = self.dropout(core_out)
         new_mem = self._update_mem(hids, mem, mlen, qlen)
         return core_out, new_mem
 

transformers/transformer_wrapper.py
@@ -122,8 +122,8 @@ class Transformer(nn.Module):
         """
         if self.transformer is None: return inputs
         if self.transformer_type == "xl" or self.transformer_type == "gtrxl":
-            ret = self.transformer(inputs, *self.mem)
-            output, self.mem = ret[0], ret[1:]
+            output, self.mem = self.transformer(inputs, self.mem)
+            # output, self.mem = ret[0], ret[1:]
         elif self.transformer_type == "linformer":
             output = self.transformer(inputs)
             output = self.out_layer(output)