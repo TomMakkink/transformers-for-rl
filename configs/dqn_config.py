2b30d787ebc3ea37e946884671068bafc2f09eec
Tom Makkink <t.makkink@instadeep.com>
2020-09-02 13:20:14 +0200

    Fix: cleaned dqn config

--- configs/dqn_config.py
+++ configs/dqn_config.py
@@ -1,8 +1,6 @@
 dqn_config = {
-    "max_steps_per_episode": 2048,
     "lr": 0.0001,
     "gamma": 0.99,
-    "log_interval": 10,
     "buffer_size": 100,
     "batch_size": 32,
     "epsilon": {"start": 1.0, "final": 0.01, "decay": 500},
