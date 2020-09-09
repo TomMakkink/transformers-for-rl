Fix: window padding and uses nn.GRU units

graphs.py
@@ -6,17 +6,18 @@ from os.path import isfile, join, splitext
 import argparse
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 sns.set()
 
-
-DEFAULT_SAVE_DIR = 'graphs'
-DEFAULT_DATA_FOLDER = 'results'
-ALGORITHMS = ['A2C']
-MODELS = ['rezero', 'gtrxl', 'vanilla', 'xl', 'LSTM', 'none']
-ENVS = ['memory_len', 'memory_size', 'cartpole']
-ENVS_NUM = ['0', '1', '2', '3', '4', '5']
-SEEDS = ['28', '61', '39', '78', '72', '46', '61', '71', '93', '44']
-COLOURS = ['blue', 'green', 'red', 'purple', 'black', 'orange']
+DEFAULT_SAVE_DIR = "graphs"
+DEFAULT_DATA_FOLDER = "results"
+ALGORITHMS = ["A2C"]
+MODELS = ["mha", "rmha", "lmha", "gmha", "lstm", "none"]
+ENVS = ["memory_len", "memory_size"]
+ENVS_NUM = ["0", "1", "2", "3", "4", "5"]
+SEEDS = ["28", "61", "39", "78", "72", "46", "61", "71", "93", "44"]
+COLOURS = ["blue", "green", "red", "purple", "black", "orange"]
+# WINDOW = ["5", "10", "25", "50"]
 
 # - results
 #     - env
@@ -41,18 +42,17 @@ def process_data(root, save):
                     returns = []
                     episodes = []
                     for seed in SEEDS:
-                        results_dir = '/'.join([root, env,
-                                                env_num, algo, model, seed])
+                        results_dir = "/".join([root, env, env_num, algo, model, seed])
                         if os.path.exists(results_dir):
                             results_csv = listdir(results_dir)
                             if len(results_csv) == 1:
-                                results_path = '/'.join([results_dir,
-                                                         results_csv[0]])
+                                results_path = "/".join([results_dir, results_csv[0]])
                                 data = np.genfromtxt(
-                                    results_path, dtype=float, delimiter=',', names=True)
+                                    results_path, dtype=float, delimiter=",", names=True
+                                )
                                 # print(
                                 #     f"Shape of episode_return: {data['episode_return'].shape}")
-                                returns.append(data['episode_return'])
+                                returns.append(data["episode_return"])
                                 episodes.append(data["episode"])
 
                     # Assume episodes are all the same
@@ -61,16 +61,18 @@ def process_data(root, save):
                     std = calculate_std(returns)
                     if len(x) == len(y):
                         ax.plot(
-                            x, y, '-', color=COLOURS[colour_index], label=f"{model}")
-                        # ax.fill_between(x, y - std, y + std,
-                        #                 color=COLOURS[colour_index], alpha=0.2)
+                            x, y, "-", color=COLOURS[colour_index], label=f"{model}"
+                        )
+                        ax.fill_between(
+                            x, y - std, y + std, color=COLOURS[colour_index], alpha=0.2
+                        )
                         colour_index = colour_index + 1
                     else:
                         print(
-                            f"Skippping file due to mismatch sizes between x: {len(x)} and y: {len(y)}")
+                            f"Skippping file due to mismatch sizes between x: {len(x)} and y: {len(y)}"
+                        )
                 ax.legend()
-                fig.savefig(
-                    f"{save}/{algo}_{env}_{env_num}.pdf", format='pdf')
+                fig.savefig(f"{save}/{algo}_{env}_{env_num}.pdf", format="pdf")
 
 
 def calculate_mean(data):

models/memory.py
@@ -23,6 +23,9 @@ class Memory(nn.Module):
 
     def __init__(self, memory_type, input_dim, output_dim):
         super(Memory, self).__init__()
+        self.memory = None
+        self.memory_type = None
+
         if memory_type is not None:
             self.memory_type = memory_type.lower()
             print(f"Using {self.memory_type}...")
@@ -47,9 +50,6 @@ class Memory(nn.Module):
                 submodule = get_transformer_submodule(self.memory_type)
                 self.mem = tuple()
                 self.memory = MemoryTransformerModel(input_dim, output_dim, submodule)
-        else:
-            self.memory = None
-            self.memory_type = None
 
     def forward(self, x):
         """
@@ -78,5 +78,5 @@ class Memory(nn.Module):
                 ),
             )
         elif type(self.memory) == MemoryTransformerModel:
-            self.mem = self.memory.init_mem()
+            self.mem = self.memory.reset()
 

transformers/transformer.py
@@ -12,6 +12,8 @@ from transformers.attention_layer import (
 )
 from transformers.positional_encoding_layer import PositionalEncoding
 from configs.transformer_config import transformer_config
+from configs.lstm_config import lstm_config
+from configs.experiment_config import experiment_config
 
 Tensor = torch.Tensor
 
@@ -106,6 +108,10 @@ class MemoryTransformerModel(nn.Module):
             ]
         )
 
+        self.gru_hidden_state = torch.zeros(1, 1, lstm_config["hidden_dim"]).to(
+            experiment_config["device"]
+        )
+
         self.output_layer = nn.Linear(d_model, output_dim, bias=False)
 
     def init_mem(self):
@@ -167,8 +173,14 @@ class MemoryTransformerModel(nn.Module):
         hids.append(core_out)
         for i, layer in enumerate(self.MemoryTransformerLayers):
             mem_i = None if mem is None else mem[i]
-            core_out = layer(
-                core_out, pos_emb, self.u, self.v, attn_mask=None, mem=mem_i
+            core_out, self.gru_hidden_state = layer(
+                core_out,
+                pos_emb,
+                self.u,
+                self.v,
+                attn_mask=None,
+                mem=mem_i,
+                hidden=self.gru_hidden_state,
             )
             hids.append(core_out)
 
@@ -177,6 +189,12 @@ class MemoryTransformerModel(nn.Module):
         new_mem = self._update_mem(hids, mem, mlen, qlen)
 
         return core_out, new_mem
+
+    def reset(self):
+        self.init_mem()
+        self.gru_hidden_state = torch.zeros(1, 1, lstm_config["hidden_dim"]).to(
+            experiment_config["device"]
+        )
 
 
 class TransformerBlockBase(nn.Module):
@@ -392,8 +410,9 @@ class RMHA(TransformerBlockBase):
         v: Tensor,
         attn_mask: Tensor = None,
         mem: Tensor = None,
+        hidden: Tensor = None,
     ):
-        return self.attention(inputs, r, u, v, attn_mask, mem)
+        return self.attention(inputs, r, u, v, attn_mask, mem), None
 
 
 class GMHA(TransformerBlockBase):
@@ -405,7 +424,10 @@ class GMHA(TransformerBlockBase):
             dropout,
             mem_len=transformer_config["mem_len"],
         )
-        self.gated_layer = GatedRecurrentUnit(d_model)
+        # self.gated_layer = GatedRecurrentUnit(d_model)
+        self.gated_layer = nn.GRU(
+            d_model, lstm_config["hidden_dim"], num_layers=lstm_config["num_layers"]
+        )
 
     def forward(
         self,
@@ -415,10 +437,11 @@ class GMHA(TransformerBlockBase):
         v: Tensor,
         attn_mask: Tensor = None,
         mem: Tensor = None,
+        hidden: Tensor = None,
     ):
         y = self.attention(inputs, r, u, v, attn_mask, mem)
-        y = self.gated_layer([inputs, y])
-        return y
+        y, hidden = self.gated_layer(y, hidden)
+        return y, hidden
 
 
 def get_transformer_submodule(transformer: str):